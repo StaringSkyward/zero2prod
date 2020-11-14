fdf22e93c88a844f4e6d09a19c23a4cf916b797b
Matt Law <staringskyward@gmail.com>
2020-11-14 19:08:11 +0000

    Refactored for integration tests and added the first test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    let listener = TcpListener::bind("0.0.0.0:8080")
+        .expect("Failed to bind to port 8080");
+    run(listener)?.await
 }
