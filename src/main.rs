24c8e75916d05028d0de6520e6b351e8347e1be8
Matt Law <staringskyward@gmail.com>
2020-11-14 19:39:25 +0000

    Fix rustfmt issues

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use zero2prod::run;
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    let listener = TcpListener::bind("0.0.0.0:8080")
-        .expect("Failed to bind to port 8080");
+    let listener = TcpListener::bind("0.0.0.0:8080").expect("Failed to bind to port 8080");
     run(listener)?.await
 }
