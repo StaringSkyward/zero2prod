ec2978449f49bd9157fd20cb3e0e6ff648353ca9
Matt Law <staringskyward@gmail.com>
2020-12-04 12:50:35 +0000

    Remove code coverage build task as tarpaulin won't compile. Timeout after 60 seconds of trying to connect to postgres DB.

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,38 +2,48 @@
 set -x
 set -eo pipefail
 
-DB_USER=${POSTGRES_USER:=postgres}
+# Check if a custom DB host has been set, otherwise default to localhost
+DB_HOST="${POSTGRES_HOST:=localhost}"
+# Check if a custom user has been set, otherwise default to 'postgres'
+DB_USER="${POSTGRES_USER:=postgres}"
+# Check if a custom password has been set, otherwise default to 'password'
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+# Check if a custom DB has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=newsletter}"
+# Check if a custom port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
 
-# Allow us to skip the call to docker run if a database container is already running
+# Allow to skip Docker if a dockerized Postgres database is already running
 if [[ -z "${SKIP_DOCKER}" ]]
 then
-  # Launch postgres using Docker with a fairly high
-  # maximum number of connections for testing purposes
+  # Launch postgres using Docker
   docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}:${DB_PORT}" \
-    -d postgres \
-    postgres -N 1000
+      -e POSTGRES_USER=${DB_USER} \
+      -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+      -e POSTGRES_DB=${DB_NAME} \
+      -p "${DB_PORT}":5432 \
+      -d postgres \
+      postgres -N 200
+      # ^ Increased maximum number of connections for testing purposes
 fi
 
 # Keep pinging Postgres until it's ready to accept commands
-export PGPASSWORD="${DB_PASSWORD}"
-until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
-  >&2 echo "Postgres is still unavailable - sleeping"
-  sleep 1
+WAITED=0
+until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d postgres -c '\q'; do
+  >&2 echo "Postgres is still unavailable after ~${WAITED} seconds - sleeping";
+  WAITED=`expr $WAITED + 2`;
+  if (($WAITED > 60)); then
+    >&2 echo "Timed out after waiting 60 seconds for Postgres to become available!";
+    exit 1;
+  fi
+  sleep 2;
 done
 
->&2 echo "Postgres is up and running on port ${DB_PORT}!"
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
->&2 echo "Creating Postgres DB..."
-export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 sqlx database create
-
->&2 echo "Running migrations..."
-mkdir -p migrations
 sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
+
