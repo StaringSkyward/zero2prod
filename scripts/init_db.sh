d3137ed9f565d38cb96b1339f61f12a31a6f7fc9
Matt Law <staringskyward@gmail.com>
2020-12-04 13:12:23 +0000

    Fixed init_db.sh, added caching to github action

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -16,7 +16,7 @@ then
     -e POSTGRES_USER=${DB_USER} \
     -e POSTGRES_PASSWORD=${DB_PASSWORD} \
     -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432" \
+    -p "${DB_PORT}":5432 \
     -d postgres \
     postgres -N 1000
 fi
