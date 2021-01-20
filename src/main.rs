34783498f9ba00b7e376387a5d09addbaf32ae78
Matt Law <staringskyward@gmail.com>
2021-01-20 18:56:34 +0000

    Trying to get tests to pass on github

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    openssl_probe::init_ssl_cert_env_vars();
-
     let subscriber = get_subscriber("zero2prod".into(), "info".into());
     init_subscriber(subscriber);
 
